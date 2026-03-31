e2a21645962f32bc9cb285bcad4dc780bf41bd28
Larissa <lala.objetivo@gmail.com>
2026-03-30 22:00:54 -0300

    Exercício 1 - Larissa E Nathan

--- exercicios/33-funcoes/larissa&nathan/Exerc01.js
+++ exercicios/33-funcoes/larissa&nathan/Exerc01.js
@@ -4,5 +4,5 @@ const apresentar = (nome, curso, semestre = 1) => {
 };
 
 // Testes
-console.log(apresentar("Larissa", "Eng. Computação", 3));
+console.log(apresentar("Lari", "Eng. Computação", 3));
 console.log(apresentar("Nathan", "Eng. Computação"));
